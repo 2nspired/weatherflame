7eb51e01d94a3596e65922c74426a2dcb4596abb
Thomas Trudzinski <thomastrudzinski@gmail.com>
2024-01-14 17:27:37 -1000

    new weather widget layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">Weather App</div>
+    <main className="flex flex-col items-center justify-between">
+      <div className="mt-24 rounded p-5 shadow-lg">
+        <div className="py-2 text-lg">City</div>
+        <div className="py-4 text-lg">State</div>
+        <div className="py-2 text-sm">latitude and longitude</div>
+        <div className="py-6 text-2xl">temperature</div>
+        <div className="py-4 text-base">feels like</div>
+        <div className="py-2 text-sm">wind speed</div>
+        <div className="py-2 text-base">short description</div>
+      </div>
     </main>
   );
 }
