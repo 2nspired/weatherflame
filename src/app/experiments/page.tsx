eaa820a8bddebc9b609f9637ab8baecd06eb3767
Thomas Trudzinski <thomastrudzinski@gmail.com>
2024-12-02 20:58:08 -1000

    clean up

---
+++ src/app/experiments/page.tsx
@@ -0,0 +1,35 @@
+// import Footer from "./_components/Footer";
+// import Header from "./_components/Header";
+
+import Landing from "~/app/main/_components/Landing";
+import { getLocationByZip, getLocationByName } from "~/server/api/location";
+import pluginReact, { rules } from "eslint-plugin-react";
+
+export default async function Experiments() {
+  // console.log(
+  //   "BY ZIP ------->>>>> RUN",
+  //   await getLocationByZip({ zip: "90210", countryCode: "us" }),
+  // );
+
+  // console.log(
+  //   "BYNAME ------->>>>>",
+  //   await getLocationByName({
+  //     city: "san antonio",
+  //     stateCode: "tx",
+  //     countryCode: "us",
+  //   }),
+  // );
+  console.log(pluginReact.configs);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-700 font-mono text-white">
+      {/* <Header /> */}
+      Main Page
+      {/* Content */}
+      <div className="flex flex-col items-center justify-center">
+        <Landing />
+      </div>
+      <div className="mt-12 pl-3">coming soon...</div>
+      {/* <Footer /> */}
+    </main>
+  );
+}
