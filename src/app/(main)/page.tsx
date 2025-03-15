91147878f6d79bbb999d1e17187d37c59dbbb0f4
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-03-14 19:19:01 -1000

    tailwind css class order fixes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,16 +16,16 @@ export default async function Home() {
           {/* CONTENT */}
           <div className="flex w-full flex-col sm:flex-row">
             <div className="w-2/6">
-              <div className="hidden w-2/6 animate-fade-expand items-center justify-center border-y border-black bg-[#FF6100] text-zinc-100 sm:flex">
+              <div className="animate-fade-expand hidden w-2/6 items-center justify-center border-y border-black bg-[#FF6100] text-zinc-100 sm:flex">
                 <TypewriterText
                   text="WF°"
                   className="-mr-2 flex items-center font-mono text-[4rem] leading-none md:text-[5rem] lg:text-[7rem]"
                 />
               </div>
             </div>
-            <div className="w-full animate-fade-in border-y border-l-0 border-black text-zinc-100 opacity-0 delay-500 sm:border-l">
+            <div className="animate-fade-in w-full border-y border-l-0 border-black text-zinc-100 opacity-0 delay-500 sm:border-l">
               <div className="animate-fade-in border-b border-black p-6 opacity-0 delay-500">
-                <div className="flex animate-fade-in flex-row flex-wrap items-center font-mono text-[4rem] leading-none opacity-0 delay-500 md:text-[4rem] lg:text-[6rem]">
+                <div className="animate-fade-in flex flex-row flex-wrap items-center font-mono text-[4rem] leading-none opacity-0 delay-500 md:text-[4rem] lg:text-[6rem]">
                   <TypewriterText speed={0.05} text="WEATHER" />
                   <TypewriterText speed={0.05} timeDelay={0.3} text="FLAME" />
                 </div>
@@ -34,7 +34,7 @@ export default async function Home() {
                 Where the forecasts are hot, and mostly accurate.
               </div>
               <div>
-                <div className="h-28 animate-fade-in opacity-0 delay-1000">
+                <div className="animate-fade-in h-28 opacity-0 delay-1000">
                   <InputLocation
                     enableUserLocation={true}
                     className="flex h-full space-x-3 p-6 pb-7 sm:flex-row"
